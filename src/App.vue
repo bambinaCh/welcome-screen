c35c19d00c9163d90eff349dc8632a6b6cdc2c86
bambinaCh <chaimaa.jarite@gmail.com>
2022-05-16 16:54:34 +0200

    welcome-screen

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,47 @@
 <template>
+
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1> Welcome to Opportunity</h1>
+    <h2> 16.05.22 <h2>
+
+      <ul class="ul">
+
+        <li class="li">
+          <span style="color:red">14.00Uhr<span>
+            <h3> Besichtigungs Besuch </h3>
+              <span> Interessierte fuer den zweiten Kurs werden uns besuchen </span>
+        </li>      
+
+        <li class="li">
+          <span style="color:red">14.00Uhr<span>
+            <h3> Besichtigungs Besuch </h3>
+              <span> Interessierte fuer den zweiten Kurs werden uns besuchen </span>
+        </li> 
+
+        <li class="li">
+          <span style="color:red">14.00Uhr<span>
+            <h3> Besichtigungs Besuch </h3>
+              <span> Interessierte fuer den zweiten Kurs werden uns besuchen </span>
+        </li> 
+
+      </ul>
+
+    <footer>
+      <img class="img-footer" alt="logo" src="./assets/STZH_SEB_Logo.png">
+      <img class="img-footer2" alt="logo" src="./assets/Opportunity.png">
+      <img class="img-footer3" alt="logo" src="./SAG_Logo_De.png">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
